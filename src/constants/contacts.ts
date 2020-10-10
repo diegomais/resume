9339b853fed1049040b3e659ace994b192a3c592
Diego Mais <diegomais.dev@gmail.com>
2020-10-10 19:24:19 -0300

    ♻️ Refactor contact page

---
+++ src/constants/contacts.ts
@@ -0,0 +1,73 @@
+export default [
+  {
+    icon: 'icon-person_pin_circle',
+    label: 'Itapema, Santa Catarina (SC), Brazil',
+    target:
+      'https://www.google.com/maps/place/Itapema,+SC,+88220-000/@-27.0930705,-48.6371021,14z/data=!3m1!4b1!4m5!3m4!1s0x94d8b10cf4da0413:0x231d20d09ad9775c!8m2!3d-27.0960682!4d-48.6178074',
+    title: 'Location',
+    type: 'location'
+  },
+  {
+    icon: 'icon-mail_outline',
+    label: 'diegomais.dev@gmail.com',
+    target: 'mailto:diegomais.dev@gmail.com',
+    title: 'Email',
+    type: 'email'
+  },
+  {
+    icon: 'icon-phone',
+    label: '+55 (47) 99616-8008',
+    target: 'tel:+5547996168008',
+    title: 'Phone',
+    type: 'phone'
+  },
+  {
+    icon: 'icon-skype',
+    label: 'diego_mais',
+    target: 'skype:diego_mais?call',
+    title: 'Skype',
+    type: 'skype'
+  },
+  {
+    icon: 'icon-github',
+    label: '/diegomais',
+    target: 'http://www.github.com/diegomais',
+    title: 'GitHub',
+    type: 'github'
+  },
+  {
+    icon: 'icon-linkedin',
+    label: '/diegomais',
+    target: 'http://www.linkedin.com/diegomais',
+    title: 'LinkedIn',
+    type: 'linkedin'
+  },
+  {
+    icon: 'icon-twitter',
+    label: '@diegomais',
+    target: 'http://www.twitter.com/diegomais',
+    title: 'Twitter',
+    type: 'twitter'
+  },
+  {
+    icon: 'icon-facebook',
+    label: '/diegomais8',
+    target: 'http://www.facebook.com/diegomais8',
+    title: 'Facebook',
+    type: 'facebook'
+  },
+  {
+    icon: 'icon-instagram',
+    label: '/diegomais',
+    target: 'http://www.instagram.com/diegomais',
+    title: 'Instagram',
+    type: 'instagram'
+  },
+  {
+    icon: 'icon-globe',
+    label: 'diegomais.tk',
+    target: 'http://diegomais.tk',
+    title: 'Website',
+    type: 'website'
+  }
+]
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,6 +3,7 @@ import Footer from '../components/footer'
 import Header from '../components/header'
 import Loader from '../components/loader'
 import Nav from '../components/nav'
+import contacts from '../constants/contacts'
 
 const ContactPage: React.FC = () => (
   <>
@@ -14,75 +15,37 @@ const ContactPage: React.FC = () => (
 
       <div id="colorlib-page">
         <Header />
-        <section className="ftco-section contact-section">
+        <main className="ftco-section contact-section">
           <div className="container mt-5">
-            <div className="row d-flex mb-5 contact-info">
-              <div className="col-md-12 mb-4">
-                <h2 className="h4">Contact Information</h2>
+            <div className="row d-flex mx-1 mb-5 contact-info">
+              <div className="mb-4">
+                <h2 className="h3">Contact Information</h2>
               </div>
-              <div className="w-100"></div>
-              <div className="col-md-6">
-                <p>
-                  <span>Location:</span> <a href="#">Blumenau - SC, Brazil</a>
-                </p>
-                <p>
-                  <span>Phone:</span>{' '}
-                  <a href="tel:+5547996168008">+55 (47) 99616-8008</a>
-                </p>
-                <p>
-                  <span>Email:</span>{' '}
-                  <a href="mailto:diegomais@live.com">diegomais@live.com</a>
-                </p>
-                <p>
-                  <span>Skype:</span>{' '}
-                  <a href="skype:diego_mais?call">diego_mais</a>
-                </p>
-                <p>
-                  <span>Twitter:</span>{' '}
-                  <a href="http://www.twitter.com/diegomais">@diegomais</a>
-                </p>
-              </div>
-              <div className="col-md-6">
-                <p>
-                  <span>Website:</span>{' '}
-                  <a href="http://diegomais.github.io">diegomais.github.io</a>
-                </p>
-                <p>
-                  <span>Github:</span>{' '}
-                  <a href="http://github.com/diegomais">github.com/diegomais</a>
-                </p>
-                <p>
-                  <span>LinkedIn:</span>{' '}
-                  <a href="http://www.linkedin.com/in/diegomais">
-                    linkedin.com/in/diegomais
-                  </a>
-                </p>
-                <p>
-                  <span>Facebook:</span>{' '}
-                  <a href="http://www.facebook.com/diegomais8">
-                    facebook.com/diegomais8
-                  </a>
-                </p>
-                <p>
-                  <span>Instagram: </span>{' '}
-                  <a href="http://www.instagram.com/diegomais">
-                    instagram.com/diegomais
-                  </a>
-                </p>
+              <div className="row row-cols-3">
+                {contacts.map(item => (
+                  <div className="col-md-6 col-lg-4" key={item.type}>
+                    <p>
+                      <span className={item.icon} />{' '}
+                      <a href={item.target}>{item.label}</a>
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
             <div className="center">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227807.2014815671!2d-49.23918711962158!3d-26.856247672824423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94df1e408b5c3095%3A0xacfb8520bc1a7644!2sBlumenau%2C+SC!5e0!3m2!1spt-BR!2sbr!4v1556379111618!5m2!1spt-BR!2sbr"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28416.057202436077!2d-48.63710209969119!3d-27.0930704598793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d8b10cf4da0413%3A0x231d20d09ad9775c!2sItapema%2C%20SC%2C%2088220-000!5e0!3m2!1spt-BR!2sbr!4v1602365326036!5m2!1spt-BR!2sbr"
                 width="100%"
                 height="400"
                 frameBorder={0}
                 style={{ border: 0 }}
-                allowFullScreen
-              ></iframe>
+                allowFullScreen={false}
+                aria-hidden={false}
+                tabIndex={0}
+              />
             </div>
           </div>
-        </section>
+        </main>
         <Footer />
         <Loader />
       </div>
