cef365369b607e98fb647891792c43c0e7aa0c10
Diego Mais <diegomais.dev@gmail.com>
2020-10-10 19:34:19 -0300

    ♻️ Refactor footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,53 +1,57 @@
-import React from 'react'
-import contact from '../constants/contact'
-import social from '../constants/social'
+import React, { useMemo } from 'react'
+import contacts from '../constants/contacts'
 
-const Footer: React.FC = () => (
-  <footer className="ftco-footer ftco-bg-dark ftco-section">
-    <div className="container">
-      <div className="row mb-5 justify-content-center">
-        <div className="col-md-5 text-center">
-          <div className="ftco-footer-widget mb-5">
-            <ul className="ftco-footer-social list-unstyled">
-              {social.map(({ target, value }) => (
-                <li className="ftco-animate" key={value}>
-                  <a href={target} rel="noreferrer" target="_blank">
-                    <span className={`icon-${value}`} />
-                  </a>
-                </li>
+const Footer: React.FC = () => {
+  const social = useMemo(() => contacts.filter(item => item.social), [])
+  const footer = useMemo(() => contacts.filter(item => item.footer), [])
+
+  return (
+    <footer className="ftco-footer ftco-bg-dark ftco-section">
+      <div className="container">
+        <div className="row mb-5 justify-content-center">
+          <div className="col-md-5 text-center">
+            <div className="ftco-footer-widget mb-5">
+              <ul className="ftco-footer-social list-unstyled">
+                {social.map(({ icon, target, type }) => (
+                  <li className="ftco-animate" key={type}>
+                    <a href={target} rel="noreferrer" target="_blank">
+                      <span className={icon} />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="ftco-footer-widget">
+              <h2 className="mb-3">Contact Me</h2>
+              {footer.map(({ icon, label, target, type }) => (
+                <p key={type}>
+                  <span className={icon} /> <a href={target}>{label}</a>
+                </p>
               ))}
-            </ul>
+            </div>
           </div>
-          <div className="ftco-footer-widget">
-            <h2 className="mb-3">Contact Me</h2>
-            {contact.map(({ type, label, target, value }) => (
-              <p key={value}>
-                {type}: <a href={target}>{label}</a>
-              </p>
-            ))}
+        </div>
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <p>
+              Copyright &copy;
+              {JSON.stringify(new Date().getFullYear())} All rights reserved
+              template is made with{' '}
+              <i
+                className="icon-heart"
+                aria-hidden="true"
+                style={{ color: 'red' }}
+              ></i>{' '}
+              by{' '}
+              <a href="https://colorlib.com" rel="noreferrer" target="_blank">
+                Colorlib
+              </a>
+            </p>
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-md-12 text-center">
-          <p>
-            Copyright &copy;
-            {JSON.stringify(new Date().getFullYear())} All rights reserved
-            template is made with{' '}
-            <i
-              className="icon-heart"
-              aria-hidden="true"
-              style={{ color: 'red' }}
-            ></i>{' '}
-            by{' '}
-            <a href="https://colorlib.com" rel="noreferrer" target="_blank">
-              Colorlib
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,71 +1,91 @@
 export default [
   {
+    footer: false,
     icon: 'icon-person_pin_circle',
     label: 'Itapema, Santa Catarina (SC), Brazil',
+    social: false,
     target:
       'https://www.google.com/maps/place/Itapema,+SC,+88220-000/@-27.0930705,-48.6371021,14z/data=!3m1!4b1!4m5!3m4!1s0x94d8b10cf4da0413:0x231d20d09ad9775c!8m2!3d-27.0960682!4d-48.6178074',
     title: 'Location',
     type: 'location'
   },
   {
+    footer: true,
     icon: 'icon-mail_outline',
     label: 'diegomais.dev@gmail.com',
+    social: false,
     target: 'mailto:diegomais.dev@gmail.com',
     title: 'Email',
     type: 'email'
   },
   {
+    footer: true,
     icon: 'icon-phone',
     label: '+55 (47) 99616-8008',
+    social: false,
     target: 'tel:+5547996168008',
     title: 'Phone',
     type: 'phone'
   },
   {
+    footer: true,
     icon: 'icon-skype',
     label: 'diego_mais',
+    social: true,
     target: 'skype:diego_mais?call',
     title: 'Skype',
     type: 'skype'
   },
   {
+    footer: false,
     icon: 'icon-github',
     label: '/diegomais',
+    social: true,
     target: 'http://www.github.com/diegomais',
     title: 'GitHub',
     type: 'github'
   },
   {
+    footer: false,
     icon: 'icon-linkedin',
     label: '/diegomais',
+    social: true,
     target: 'http://www.linkedin.com/diegomais',
     title: 'LinkedIn',
     type: 'linkedin'
   },
   {
+    footer: false,
     icon: 'icon-twitter',
     label: '@diegomais',
+    social: true,
     target: 'http://www.twitter.com/diegomais',
     title: 'Twitter',
     type: 'twitter'
   },
   {
+    footer: false,
     icon: 'icon-facebook',
     label: '/diegomais8',
+    social: true,
     target: 'http://www.facebook.com/diegomais8',
     title: 'Facebook',
     type: 'facebook'
   },
   {
+    footer: false,
     icon: 'icon-instagram',
     label: '/diegomais',
+    social: true,
     target: 'http://www.instagram.com/diegomais',
     title: 'Instagram',
     type: 'instagram'
   },
   {
+    footer: false,
     icon: 'icon-globe',
     label: 'diegomais.tk',
+    social: false,
     target: 'http://diegomais.tk',
     title: 'Website',
     type: 'website'
