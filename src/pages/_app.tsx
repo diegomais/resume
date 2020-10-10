2558350169bb3c9e14ed37c9c0e53f5c271038d0
Diego Mais <diegomais.dev@gmail.com>
2020-10-10 20:11:24 -0300

    🔥 Remove style files from create next app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import React from 'react'
-import '../styles/globals.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
