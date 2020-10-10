510ef5ca213816cef86f11032e0cabcd56430916
Diego Mais <diegomais.dev@gmail.com>
2020-10-09 21:42:18 -0300

    🎨 Add EditorConfig, ESLint and Prettier
    - EditorConfig maintains consistent coding styles for multiple developers working on the same project across various editors and IDEs.
    - ESLint finds and fixes problems in JavaScript/TypeScript code.
    - Prettier is an opinionated code formatter.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import { AppProps } from 'next/app'
+import React from 'react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
 
 export default MyApp
